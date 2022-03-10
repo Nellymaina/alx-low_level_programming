03c61965d483a2625e3e4423c46df8c04b58fd3c
Nellymaina <nellymaina095@gmail.com>
2022-03-10 14:26:04 -0800

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  * Desscription: prints the sizes of different types
  * in different architectures
  *
- * Return: always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
@@ -14,7 +14,7 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 	printf("Size of a long int :%d byte(s)\n", (int) sizeof(long));
 	printf("Size of a long long int: %d byte(s)\n" (int) sizeof(long long));
-	printf("Size of a float: $d byte(S)\n", (int) sizeof(float));
-	return 0
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+	return 0;
 }
 
