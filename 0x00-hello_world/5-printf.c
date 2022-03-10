86bb66dcd85d4f69089a37fcc86b6aa5535c9bb7
Nellymaina <nellymaina095@gmail.com>
2022-03-10 14:13:50 -0800

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string to the console
+ *
+ *  Description - prints "with proper grammar, but the outcome
+ *  is a piece of art,"
+ *
+ *  Return: Always 0 (Success)
+ *  */
+int main(void)
+{
+	printf("with proper grammar, but th outcome is a piece of art, \n");
+	return (0)
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints sizes
+ *
+ * Desscription: prints the sizes of different types
+ * in different architectures
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int :%d byte(s)\n", (int) sizeof(long));
+	printf("Size of a long long int: %d byte(s)\n" (int) sizeof(long long));
+	printf("Size of a float: $d byte(S)\n", (int) sizeof(float));
+	return 0
+}
+
