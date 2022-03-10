6e9896605906794b222715ae28263d1eaec15664
Nellymaina <nellymaina095@gmail.com>
2022-03-10 14:47:09 -0800

    c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints to stderr
+ *
+ * Description: prints "and that piece of art is useful"
+ * - Dora Kopar, 2015-10-19" to the stderr
+ *
+ *   Return: 1 (Error)
+ */
+int main(void)
+{
+	fprintf(stderr,
+	"and that piece of art is useful\" - Dora Kopar, 2015-10-19\n");
+	return (1);
+}
