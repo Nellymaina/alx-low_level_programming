9a83401c2378f89838207a441e2b9ae40fc7b5f2
Nellymaina <99280364+Nellymaina@users.noreply.github.com>
2022-03-17 23:08:22 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,12 +7,11 @@
  * Return: 1 if c is a digit
  */
 
-int main(void)
+int _isdigit(int c)
 {
-    char c;
-
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
-    return (0);
+	if (c >= 0 && c <=9)
+	{
+		return (1);
+	}
+	else
+		return (0);
