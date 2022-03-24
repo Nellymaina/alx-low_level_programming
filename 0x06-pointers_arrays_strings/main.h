02aefead4d94cb39b2bacd7acf52ad4a510841e6
Nellymaina <99280364+Nellymaina@users.noreply.github.com>
2022-03-25 01:02:25 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
-char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
@@ -14,6 +11,3 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
-
-#endif
